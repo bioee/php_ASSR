9ec6fd381c38238e4722f7fc8cfe66194b3733c6
flo <flo@controller.access.network>
2018-04-25 16:40:47 +0200

    + currentime | password

--- createbase.sql
+++ createbase.sql
@@ -5,7 +5,7 @@ CREATE TABLE projet_assr_php.user
 	    id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	    login varchar(255),
 	    mail varchar(255),
-	    mot_de_passe varchar(255),
-	    date_authent DATE,
+	    password varchar(255),
+	    date_authent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	    admin BOOLEAN    
 )
