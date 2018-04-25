617cfbaf7911b04189b8bf7edd89bf523b68f1f5
root <vaca@hotmail.fr>
2018-04-25 12:52:26 +0100

    travail sur Authent

--- Main.php
+++ Main.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html> 
 <html>
-    <head>        
+    <head lang="fr">        
 	<?php include("entete.php"); ?>
     </head>
      <body>             
@@ -11,28 +11,27 @@
 	 
 	$login_valide = "moi";
 	$pwd_valide = "lemien";
-
-	include("form.php"); ?>
 	
-	*/
+
 	if (isset($_POST['login']) && isset($_POST['pwd'])) {
 
 	// on vérifie les informations du formulaire, à savoir si le pseudo saisi est bien un pseudo autorisé, de même pour le mot de passe
-	if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
-		// dans ce cas, tout est ok, on peut démarrer notre session
-
-		// on la démarre :)
-		session_start ();
-		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
-		$_SESSION['login'] = $_POST['login'];
-		$_SESSION['pwd'] = $_POST['pwd'];
-
-		// on redirige notre visiteur vers une page de notre section membre
-		header ('location: page_membre.php');
-		
-	}else {
-		#include("form.php");
-	
+  	if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
+  		// dans ce cas, tout est ok, on peut démarrer notre session
+  
+  		// on la démarre :)
+  		session_start ();
+  		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
+  		$_SESSION['login'] = $_POST['login'];
+  		$_SESSION['pwd'] = $_POST['pwd'];
+  
+  		// on redirige notre visiteur vers une page de notre section membre
+  		header ('location: page_membre.php');
+  		
+  	}else {
+  		include("form.php");
+  	}
+  }
 	?>
 	
 	  <?php include("pied.php"); ?>
--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-<form action="login.php" method="post">
+<form action="Main.php" method="post">
 Votre login : <input type="text" name="login"><br>
 Votre mot de passe : <input type="password" name="pwd"><br>
 <input type="submit" value="Connexion">
