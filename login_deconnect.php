bd8080cee3a8a66eab2673f964e434206ca35787
root <vaca@hotmail.fr>
2018-04-26 12:46:48 +0100

    login3

--- login_deconnect.php
+++ login_deconnect.php
@@ -1,19 +1,12 @@
 <?php
-// on stop la session:
+header("Location: Main.php");
+
+// on detruit la session:
 $_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
 session_destroy();
 ?> 
 
 
-
-//header("Location: Main.php");
 ?>
 
 
