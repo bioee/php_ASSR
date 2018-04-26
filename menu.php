da5782a185cb21592c2d8334f9705c3a65de941a
dreamsness <josemd@hotmail.fr>
2018-04-26 10:28:23 +0200

    Jose mise en forme menu

--- menu.php
+++ menu.php
@@ -3,30 +3,26 @@
 
             <ul>
 	            <li><a href="#" >Accueil</a></li>
-                <li><a href="#" >Toto</a></li>
                 <li><a href="#" >Aide</a></li>
 
          <?php
             if (!isset($_SESSION['login'])) :
                 ?>
+
                 <!-- s'identifier / s'inscrire -->
-                <!-- <ul> -->
                     <li><a href="form.php">S'identifier</a></li>
-                <!-- </ul> -->
 
                 <?php
             else :
                 $utilisateur = $_SESSION['login'];
                 ?>
 
-               <!-- <ul> -->
                     <li><?= $utilisateur ?></li>
                     <li> <a role='button' title="Déconnection." href="login_deconnect.php" >
                             <!-- <img src="images/deconnection.png" alt=""/> -->
                             Deconnection
                          </a>
                     </li>
-                <!-- </ul> -->
 
 
             <?php
