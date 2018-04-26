63ded83d4b36146c9d985ebb06fff220a39504eb
root <vaca@hotmail.fr>
2018-04-26 12:52:40 +0100

    login3

--- menu.php
+++ menu.php
@@ -9,8 +9,8 @@
             if (!isset($_SESSION['id'])){ // useur non authentifié:
                     echo "<li><a href=\"login_form.php\">S'identifier</a></li>" ;
             }else{
-                    echo "<li><p> bonjour ".$_SESSION['login']."</p></li>" ;
                     echo "<li> <a role='button' title=\"DÃ©connection.\" href=\"login_deconnect.php\" >Deconnection</a></li>" ;
+                    echo "<li><p> bonjour ".$_SESSION['login']."</p></li>" ;
             } 
             ?>
             </ul>
