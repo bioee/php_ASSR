a7879ad5fb69c0e13a58d7acc40f9e1441d0544a
dreamsness <josemd@hotmail.fr>
2018-04-26 10:25:13 +0200

    Jose mise en forme menu

--- Main.php
+++ Main.php
@@ -6,40 +6,11 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head lang="fr">
-	<?php include("entete.php"); ?>
+        <?php include("entete.php"); ?>
     </head>
-     <body>
-	 <?php include("menu.php"); ?>
 
-         <?php
-            if (!isset($_SESSION['login'])) :
-                ?>
-                <!-- s'identifier / s'inscrire -->
-                <ul>
-                    <li><a href="form.php">S'identifier</a></li>
-                </ul>
-
-                <?php
-            else :
-                $utilisateur = $_SESSION['login'];
-                ?>
-
-                <ul>
-                    <li><?= $utilisateur ?></li>
-                    <li> <a role='button' title="Déconnection." href="login_deconnect.php" >
-                            <!-- <img src="images/deconnection.png" alt=""/> -->
-                            Deconnection
-                         </a>
-                    </li>
-                </ul>
-
-
-            <?php
-            endif;
-            ?>
-
-
-	  <?php include("pied.php"); ?>
-
-	  </body>
+    <body>
+        <?php include("menu.php"); ?>
+        <?php include("pied.php"); ?>
+    </body>
 </html>
