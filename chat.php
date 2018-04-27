44ff2f5192330526f46ecd9ff4acd1a017ee3d33
root <vaca@hotmail.fr>
2018-04-27 11:46:16 +0100

    CHAT OK

--- chat.php
+++ chat.php
@@ -1,4 +1,33 @@
-<form action="Main.php" method="post">
+<?php     
+# connexion Ã  la base de donnÃ©es
+require 'connexionbase.php';
+
+#ajout des nouveaux messages:
+if (isset($_POST['message']) && $_POST['message'] != ""){ 
+     $query =  "INSERT INTO chat (id_msg, msg) VALUES (NULL, '".$_POST['message']."');" ;
+     $sth = $dbh->prepare($query);
+     $sth->execute();
+}            
+    
+#recuperatio et affichage des messages:
+$query =   "SELECT id_msg, msg FROM chat;" ;              
+$sth = $dbh->prepare($query);
+$sth->execute();
+if ($sth->rowCount() > 1) {
+    foreach ($sth as $row) {
+        echo "ID: ".$row['id_msg']."<br>";
+        echo "MESSAGE: ".$row['msg']."<br>";
+    }
+} elseif ($sth->rowCount() == 1) {
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
+    echo "ID: ".$result['id_msg']."<br>";
+    echo "MESSAGE: ".$result['msg']."<br>";
+}
+?>
+
+
+  <!-- Formulaire de post -->
+  <form action="Main.php" method="post">
   <p>
         <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
         <input type="submit" value="Envoyer" />
