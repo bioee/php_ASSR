f0b871fc82cf71151da06d8bdcfbc7ff9b4dc40b
root <vaca@hotmail.fr>
2018-04-27 13:44:37 +0100

    CHAT+user

--- chat.php
+++ chat.php
@@ -5,23 +5,25 @@ require 'connexionbase.php';
 #ajout des nouveaux messages:
 if (isset($_POST['message']) && $_POST['message'] != ""){ 
      $msg = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-     $query =  "INSERT INTO chat (id_msg, msg) VALUES (NULL, '".$msg."');" ;
+     $query =  "INSERT INTO chat (id_msg, user, msg) VALUES (NULL, '".$_SESSION['login']."','".$msg."');" ;
      $sth = $dbh->prepare($query);
      $sth->execute();
 }              
     
 #recuperatio et affichage des messages:
-$query =   "SELECT id_msg, msg FROM chat;" ;              
+$query =   "SELECT id_msg, user, msg FROM chat ;" ;              
 $sth = $dbh->prepare($query);
 $sth->execute();
 if ($sth->rowCount() > 1) {
     foreach ($sth as $row) {
         echo "ID: ".$row['id_msg']."<br>";
+        echo "USER: ".$row['user']."<br>";
         echo "MESSAGE: ".$row['msg']."<br>";
     }
 } elseif ($sth->rowCount() == 1) {
     $result = $sth->fetch(PDO::FETCH_ASSOC);
     echo "ID: ".$result['id_msg']."<br>";
+    echo "USER: ".$row['user']."<br>";
     echo "MESSAGE: ".$result['msg']."<br>";
 }
 ?>
--- createbase.sql
+++ createbase.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE projet_assr_php;
+CREATE USER 'user_assr_php'@'localhost' IDENTIFIED BY 'mdpASSR';
+
+
 CREATE TABLE projet_assr_php.user
     (
     id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -8,13 +11,16 @@ CREATE TABLE projet_assr_php.user
     date_authent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     admin BOOLEAN DEFAULT 0
     );
-
-CREATE USER 'user_assr_php'@'localhost' IDENTIFIED BY 'mdpASSR';
+INSERT INTO projet_assr_php.user (`id_user`, `login`, `mail`, `password`) VALUES (NULL,'user','user@user.local','user');
 GRANT ALL PRIVILEGES ON projet_assr_php.user TO 'user_assr_php'@'localhost';
 
 
 CREATE TABLE projet_assr_php.chat(
     id_msg INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user varchar(255),
     msg varchar(255)  );
-INSERT INTO projet_assr_php.chat (`id_msg`, `msg`) VALUES (NULL, 'message TEST');
+    
+    
+INSERT INTO projet_assr_php.chat (`id_msg`, `msg`) VALUES (NULL,'user_test' 'message TEST');
 GRANT ALL PRIVILEGES ON projet_assr_php.chat TO 'user_assr_php'@'localhost';
+
